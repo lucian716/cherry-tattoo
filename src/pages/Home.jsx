202b8f1ae78183e375fb8d0570f84fe2d9078327
Lucian Eugenio <lucian.aidan@gmail.com>
2023-07-21 21:11:47 -0400

    add background images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
-  return <section>{"welcome to home page"}</section>;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const images = [
+    "/images/cherry_neon.png",
+    "/images/cherry_panther.png",
+    "/images/tatouage_cerise.png",
+  ];
+
+  useEffect(() => {
+    const changeBackgroundImage = () => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    const timer = setInterval(changeBackgroundImage, 5000);
+
+    return () => clearInterval(timer);
+  }, [images.length]);
+
+  const prevImage = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+  };
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  return (
+    <section className="home-banner">
+      <div className="image-carousel">
+        <div className="arrow-button left" onClick={prevImage}>
+          &lt;
+        </div>
+        <div
+          className="image-slide"
+          style={{
+            backgroundImage: `url(${images[currentIndex]})`,
+          }}
+        />
+        <div className="arrow-button right" onClick={nextImage}>
+          &gt;
+        </div>
+      </div>
+      <div className="home-content">{/* Add your content here */}</div>
+    </section>
+  );
 };
 
 export default Home;
