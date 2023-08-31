5aac8836d6f4a1e5d237507907e40adbe682ea33
Lucian Eugenio <lucian.aidan@gmail.com>
2023-08-31 16:28:57 -0400

    add new artists

---
+++ src/pages/Artists/Rita.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "../../styles/artists.css";
+
+const Rita = () => {
+  const images = [
+    "chain_dice.png",
+    "girl_reaper.png",
+    "snake_black-grey.png",
+    "snake_colour.png",
+  ];
+
+  return (
+    <div className="artist-page">
+      <h1>Rita</h1>
+      <div className="photo-grid">
+        {images.map((image, index) => (
+          <div key={index} className="photo-item">
+            <img src={`/images_artists/${image}`} alt={`${index}`} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Rita;
