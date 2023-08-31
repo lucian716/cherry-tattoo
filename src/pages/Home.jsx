2320e348ae65454fe107e7863cc88ddcb1dd9b78
Lucian Eugenio <lucian.aidan@gmail.com>
2023-08-31 19:10:07 -0400

    del  carousel & add mp4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-import PhotoCarousel from "../pages/PhotoCarousel";
+import "../styles/video.css";
+// import PhotoCarousel from "../pages/PhotoCarousel";
+
+// const Home = () => {
+//   const images = [
+//     "/images/cherry_neon.png",
+//     "/images/cherry_panther.png",
+//     "/images/cherry_tattoo.png",
+//   ];
+
+//   return (
+//     <div>
+//       <PhotoCarousel images={images} />
+//     </div>
+//     <div className="video-container">
+//       <video autoPlay loop muted>
+//         <source src="/images/cherry-mp4" type="video/quicktime" />
+//         Your browser does not support the video tag.
+//       </video>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+
+
 
 const Home = () => {
-  const images = [
-    "/images/cherry_neon.png",
-    "/images/cherry_panther.png",
-    "/images/cherry_tattoo.png",
-  ];
-
   return (
-    <div>
-      <PhotoCarousel images={images} />
+    <div className="video-container">
+      <video autoPlay loop muted>
+        <source src="/images/cherry.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 };
