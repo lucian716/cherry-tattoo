de91e14b1eec426d03c15dd1c3478339485e93d2
Lucian Eugenio <lucian.aidan@gmail.com>
2023-10-11 17:01:23 -0400

    troubleshooting style issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,31 +2,6 @@ import React from "react";
 import "../styles/video.css";
 // import PhotoCarousel from "../pages/PhotoCarousel";
 
-// const Home = () => {
-//   const images = [
-//     "/images/cherry_neon.png",
-//     "/images/cherry_panther.png",
-//     "/images/cherry_tattoo.png",
-//   ];
-
-//   return (
-//     <div>
-//       <PhotoCarousel images={images} />
-//     </div>
-//     <div className="video-container">
-//       <video autoPlay loop muted>
-//         <source src="/images/cherry-mp4" type="video/quicktime" />
-//         Your browser does not support the video tag.
-//       </video>
-//     </div>
-//   );
-// };
-
-// export default Home;
-
-
-
-
 const Home = () => {
   return (
     <div className="video-container">
@@ -39,3 +14,25 @@ const Home = () => {
 };
 
 export default Home;
+
+// const Home = () => {
+//   const images = [
+//     "/images/cherry_neon.png",
+//     "/images/cherry_panther.png",
+//     "/images/cherry_tattoo.png",
+//   ];
+
+//   return (
+//     <div>
+//       <PhotoCarousel images={images} />
+//       <div className="video-container">
+//         <video autoPlay loop muted>
+//           <source src="/images/cherry-mp4" type="video/mp4" />
+//           Your browser does not support the video tag.
+//         </video>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Home;
