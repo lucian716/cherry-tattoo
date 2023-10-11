5ffb98f538fe9fac8786d3bbc40eda6b8e5bffcd
Lucian Eugenio <lucian.aidan@gmail.com>
2023-10-11 17:20:14 -0400

    add to header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,12 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import { FaInstagram, FaEnvelope } from "react-icons/fa";
-// import cherryGif from "./cherry_rotating.gif";
-// import "../styles/header.css";
-
-// const Header = () => {
-//   return (
-//     <header className="header">
-//       <img src={cherryGif} alt="Rotating Cherry" className="rotating-cherry" />
-//       <nav className="nav-shift">
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           <li>
-//             <Link to="/artists">Artists</Link>
-//           </li>
-//           <li>
-//             <Link to="/contact">Contact</Link>
-//           </li>
-//         </ul>
-//       </nav>
-//       <div className="header-icons">
-//         <a
-//           href="https://www.instagram.com/cherrytattoo.mtl/"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <FaInstagram className="icon" />
-//         </a>
-//         <a
-//           href="mailto:example@example.com"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           <FaEnvelope className="icon" />
-//         </a>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Header;
-
 import React from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { FaInstagram, FaEnvelope } from "react-icons/fa";
 import cherryGif from "./cherry_rotating.gif";
 import "../styles/header.css";
 
 const Header = () => {
-  const location = useLocation();
-  const isArtistsOrContactPage =
-    location.pathname === "/artists" || location.pathname === "/contact";
-
   return (
-    <header className={`header ${isArtistsOrContactPage ? "show-cherry" : ""}`}>
+    <header className="header">
       <img src={cherryGif} alt="Rotating Cherry" className="rotating-cherry" />
       <nav className="nav-shift">
         <ul>
@@ -91,4 +42,53 @@ const Header = () => {
 };
 
 export default Header;
+
+// import React from "react";
+// import { Link, useLocation } from "react-router-dom";
+// import { FaInstagram, FaEnvelope } from "react-icons/fa";
+// import cherryGif from "./cherry_rotating.gif";
+// import "../styles/header.css";
+
+// const Header = () => {
+//   const location = useLocation();
+//   const isArtistsOrContactPage =
+//     location.pathname === "/artists" || location.pathname === "/contact";
+
+//   return (
+//     <header className={`header ${isArtistsOrContactPage ? "show-cherry" : ""}`}>
+//       <img src={cherryGif} alt="Rotating Cherry" className="rotating-cherry" />
+//       <nav className="nav-shift">
+//         <ul>
+//           <li>
+//             <Link to="/">Home</Link>
+//           </li>
+//           <li>
+//             <Link to="/artists">Artists</Link>
+//           </li>
+//           <li>
+//             <Link to="/contact">Contact</Link>
+//           </li>
+//         </ul>
+//       </nav>
+//       <div className="header-icons">
+//         <a
+//           href="https://www.instagram.com/cherrytattoo.mtl/"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <FaInstagram className="icon" />
+//         </a>
+//         <a
+//           href="mailto:example@example.com"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           <FaEnvelope className="icon" />
+//         </a>
+//       </div>
+//     </header>
+//   );
+// };
+
+// export default Header;
 
